beb813401ba37dafc13fecd11bec5a656140c4d0
Yijun Mao <yijun.mao@veeva.com>
2014-11-06 13:44:30 -0800

    adding JSON marshaller so make transfer JSON object between client and server similar like JAXB

---
+++ src/main/java/com/restful/shop/annotations/Pretty.java
@@ -0,0 +1,14 @@
+package com.restful.shop.annotations;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by yijunmao on 11/6/14.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Pretty {
+
+}
